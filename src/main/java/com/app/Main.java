a4b7c44d2d337c02ded32e03a7ff33745ce18a1b
Aleksey Petrik <alexey.petric@gmail.com>
2022-03-17 19:51:12 +1000

    refactoring, class BankStatementProcessor for analysis total amounts.

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -4,22 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
     private static final String DIRECTORY = "src/main/storage/";
+    private static final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
 
     public static void main(String[] args) throws IOException {
-
-        BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
 
         Path path = Paths.get(DIRECTORY + "transactions.txt");
 
         List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(Files.readAllLines(path));
-        System.out.println(calculateTotalAmount(bankTransactions));
-    }
+        BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
-    public static double calculateTotalAmount(List<BankTransaction> bankTransactions){
-        return bankTransactions.stream().map(BankTransaction::getAmount).reduce(Double::sum).get();
+        System.out.println("All - " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("Month - " + bankStatementProcessor.calculateTotalInMonth(LocalDate.of(2021, 12, 1)));
+        System.out.println("Category - " + bankStatementProcessor.calculateTotalForCategory("pepsi"));
     }
 }
